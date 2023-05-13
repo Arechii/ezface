180d0d1abeb312c125150af7025a927a30594e40
Arechi <aglimbenli@gmail.com>
2023-05-13 21:54:01 +0200

    Stop using docker for app

--- src/server/api/routers/deepface.ts
+++ src/server/api/routers/deepface.ts
@@ -21,7 +21,7 @@ export const deepfaceRouter = createTRPCRouter({
   represent: publicProcedure.input(Input).mutation(async ({ input }) => {
     for (const image of input.images) {
       const { data } = await axios.post<unknown>(
-        "http://deepface:5000/represent",
+        "http://localhost:5000/represent",
         {
           img: image,
           model_name: input.model,
