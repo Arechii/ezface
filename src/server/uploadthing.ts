cc9c8c260f7a185241b780e95ea5c163ec062e46
Arechi <aglimbenli@gmail.com>
2023-05-13 20:24:12 +0200

    Add image entity

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,13 +1,20 @@
 /** server/uploadthing.ts */
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+import { prisma } from "./db";
 const f = createUploadthing();
 
 export const faceRouter = {
   imageUploader: f
     .fileTypes(["image"])
     .maxSize("16MB")
-    .onUploadComplete(({ file }) => {
-      console.log("file", file);
+    .onUploadComplete(async ({ file }) => {
+      await prisma.image.create({
+        data: {
+          name: file.name,
+          url: file.url,
+          label: file.name.split(".")[0] ?? file.name,
+        },
+      });
     }),
 } satisfies FileRouter;
 
