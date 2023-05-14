9011b9fc0fdd98116db4f91ccab682475112f694
Arechi <aglimbenli@gmail.com>
2023-05-14 15:31:39 +0200

    Refactor routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,44 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { deepfaceRouter } from "./routers/deepface";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  DATABASES,
+  DETECTORS,
+  MODELS,
+  SIMILARITY_METRICS,
+} from "~/utils/constants";
+import { represent } from "~/utils/deepface";
+import { fetchImage } from "~/utils/image";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
-export const appRouter = createTRPCRouter({
-  deepface: deepfaceRouter,
+const Input = z.object({
+  images: z.array(
+    z.object({
+      label: z.string().regex(/^[a-zA-Z0-9-_]+$/),
+      url: z.string().url(),
+    })
+  ),
+  model: z.enum(MODELS),
+  detector: z.enum(DETECTORS),
+  similarityMetric: z.enum(SIMILARITY_METRICS),
+  database: z.enum(DATABASES),
 });
 
-// export type definition of API
+export const appRouter = createTRPCRouter({
+  represent: publicProcedure.input(Input).mutation(async ({ input }) => {
+    for (const image of input.images) {
+      const data = await represent(
+        await fetchImage(image.url),
+        input.model,
+        input.detector
+      );
+      console.log(data);
+    }
+  }),
+  find: publicProcedure.input(Input).mutation(async ({ input }) => {
+    for (const image of input.images) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(image);
+    }
+  }),
+});
+
 export type AppRouter = typeof appRouter;
