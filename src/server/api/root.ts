82e6044d5db1195eb971297ac4ea7352b4c94f0f
Arechi <aglimbenli@gmail.com>
2023-05-14 18:10:05 +0200

    Add output for find

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { type Image } from "@prisma/client";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import {
@@ -10,6 +11,8 @@ import {
 import { represent } from "~/utils/deepface";
 import { fetchImage } from "~/utils/image";
 
+type ImageFindResult = Pick<Image, "label" | "url"> & { distance: number };
+
 const Input = z.object({
   images: z.array(
     z.object({
@@ -21,6 +24,24 @@ const Input = z.object({
   detector: z.enum(DETECTORS),
   distanceMetric: z.enum(DISTANCE_METRICS),
   database: z.enum(DATABASES),
+});
+
+const Output = z.object({
+  results: z.array(
+    z.object({
+      label: z.string().regex(/^[a-zA-Z0-9-_]+$/),
+      url: z.string().url(),
+      time: z.number(),
+      precision: z.number(),
+      matches: z.array(
+        z.object({
+          label: z.string().regex(/^[a-zA-Z0-9-_]+$/),
+          url: z.string().url(),
+          distance: z.number(),
+        })
+      ),
+    })
+  ),
 });
 
 export const appRouter = createTRPCRouter({
@@ -58,12 +79,17 @@ export const appRouter = createTRPCRouter({
     ),
   find: publicProcedure
     .input(Input)
+    .output(Output)
     .mutation(
       async ({
         input: { images, model, detector, database, distanceMetric },
         ctx: { prisma },
       }) => {
+        const results: z.infer<typeof Output>["results"] = [];
+
         for (const { label, url } of images) {
+          let images: ImageFindResult[] = [];
+          const start = Date.now();
           const embedding = JSON.stringify(
             await represent(await fetchImage(url), model, detector)
           );
@@ -73,21 +99,31 @@ export const appRouter = createTRPCRouter({
               const treshold = TRESHOLDS[model][distanceMetric];
               const query =
                 distanceMetric === "Euclidean"
-                  ? prisma.$queryRaw`
+                  ? prisma.$queryRaw<ImageFindResult[]>`
                     SELECT label, url, embedding <-> ${embedding}::vector AS distance 
                     FROM "Image" 
                     WHERE model = ${model} AND detector = ${detector} AND embedding <-> ${embedding}::vector <= ${treshold} 
                     ORDER BY distance`
-                  : prisma.$queryRaw`
+                  : prisma.$queryRaw<ImageFindResult[]>`
                     SELECT label, url, embedding <=> ${embedding}::vector AS distance 
                     FROM "Image" 
                     WHERE model = ${model} AND detector = ${detector} AND embedding <=> ${embedding}::vector <= ${treshold} 
                     ORDER BY distance`;
-              const images = await query;
-              console.log(images);
+              images = await query;
               break;
           }
+
+          results.push({
+            label,
+            url,
+            time: (Date.now() - start) / 1000,
+            precision:
+              images.filter((i) => i.label === label).length / images.length,
+            matches: images,
+          });
         }
+
+        return { results };
       }
     ),
 });
